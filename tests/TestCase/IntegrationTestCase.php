02976f0b77b00cf334c61030d239893bc69d08b2
djfm <fm.de.jouvencel@gmail.com>
2015-10-11 15:46:11 +0200

    [*] Tests : Add employee to this weird context

--- tests/TestCase/IntegrationTestCase.php
+++ tests/TestCase/IntegrationTestCase.php
@@ -4,11 +4,14 @@
 namespace PrestaShop\PrestaShop\Tests\TestCase;
 
 use PHPUnit_Framework_TestCase;
+use Employee;
+use Context;
 
 class IntegrationTestCase extends PHPUnit_Framework_TestCase
 {
     public static function setUpBeforeClass()
     {
         require_once(_PS_CONFIG_DIR_ . '/config.inc.php');
+        Context::getContext()->employee = new Employee;
     }
 }
